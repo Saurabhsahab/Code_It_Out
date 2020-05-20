0e5df558ff4553abf67d2c07970780be2105716f
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-05-20 15:37:17 +0530

    Template which I am currently using in competitions
    Template which I am currently using in competitions

--- MCT.cpp
+++ MCT.cpp
@@ -1,6 +1,7 @@
 //Saurabhsahab
 //#include <boost/math/common_factor.hpp> 
 #include<bits/stdc++.h>
+using namespace std;
 #define ll long long 
 #define lcm(x,y) boost::math::lcm(x,y)
 #define mod 1000000007
@@ -18,10 +19,10 @@
 #define f(i,s,n) for(ll i=s;i<n;i++)
 #define f1(i,s,n) for(ll i=s;i>n;i--)
 #define f2(s) for(auto E : s)
-#define AI(n) ll a[n]; f(i,0,n)cin>>a[i]
-#define AO(n) f(i,0,n)cout<<a[i]<<" "
+#define AI(a,n) ll a[n]; f(i,0,n)cin>>a[i]
+#define AO(a,n) f(i,0,n)cout<<a[i]<<" "
 #define T ll t; cin>>t; while(t--)
-using namespace std ;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
