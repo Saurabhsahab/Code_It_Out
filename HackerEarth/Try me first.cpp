c4262d3695335c7090ef22ace1a42bee0c8fed32
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-05-25 19:34:57 +0530

    https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/a-95/submissions/
    https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/a-95/submissions/

---
+++ HackerEarth/Try me first.cpp
@@ -0,0 +1,43 @@
+//Saurabhsahab
+//#include <boost/math/common_factor.hpp> 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define  V vector<ll>
+#define M map 
+#define UM unordered_map
+#define MM multimap 
+#define S set<ll>
+#define US unordered_set<ll>
+#define pb push_back
+#define DO greater<ll>()
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define stf shrink_to_fit()
+#define f(i,s,n) for(ll i=s;i<n;i++)
+#define f1(i,s,n) for(ll i=s;i>n;i--)
+#define f2(s) for(auto E : s)
+#define AI(a,n) ll a[n]; f(i,0,n)cin>>a[i]
+#define AO(a,n) f(i,0,n)cout<<a[i]<<" "
+#define T ll t; cin>>t; while(t--)
+#define TC cerr<<"\nTime elapsed: "<<1000 * clock() / CLOCKS_PER_SEC <<"ms\n";
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll n,s=0,k;
+    cin>>n;
+    f(i,0,32){
+       if(n&(1<<i))
+       k=i;
+    }
+    f(i,0,k){
+        if(!(n&(1<<i))){
+           s=s|(1<<i);
+       }
+    }
+    cout<<n<<" "<<s;
+}
