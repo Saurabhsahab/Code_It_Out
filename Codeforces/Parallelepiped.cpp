c58251c1f8d1a4b57c3c163e4d1ec2c8e0cf14ee
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-05-28 14:32:27 +0530

    http://codeforces.com/problemset/problem/224/A
    http://codeforces.com/problemset/problem/224/A

---
+++ Codeforces/Parallelepiped.cpp
@@ -0,0 +1,36 @@
+//Saurabhsahab
+//#include <boost/math/common_factor.hpp> 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define  V vector<ll>
+#define M map 
+#define UM unordered_map
+#define MM multimap 
+#define S set<ll>
+#define US unordered_set<ll>
+#define pb push_back
+#define DO greater<ll>()
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define stf shrink_to_fit()
+#define f(i,s,n) for(ll i=s;i<n;i++)
+#define f1(i,s,n) for(ll i=s;i>n;i--)
+#define f2(s) for(auto E : s)
+#define AI(a,n) ll a[n]; f(i,0,n)cin>>a[i]
+#define AO(a,n) f(i,0,n)cout<<a[i]<<" "
+#define T ll t; cin>>t; while(t--)
+#define TC cerr<<"\nTime elapsed: "<<1000 * clock() / CLOCKS_PER_SEC <<"ms\n";
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll a,b,c;
+    cin>>a>>b>>c;
+    double x=sqrt((a*b)/c),y=sqrt((b*c)/a),z=sqrt((c*a)/b);
+    cout<<4*(x+y+z);
+    
+}
