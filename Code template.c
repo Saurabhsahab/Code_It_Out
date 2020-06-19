313a1eadb839e89d5fb73b41eeeede26797d6b97
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-06-20 00:59:07 +0530

    Head Start :zap:

---
+++ Code template.c
@@ -0,0 +1,40 @@
+//SAURABHSAHAB
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#define ll long long 
+#define ull unsigned long long 
+#define ui unsigned int 
+#define ldb long double 
+#define mod 1000000007
+#define ii(u) scanf("%d",&u);
+#define iui(u) scanf("%ud",&u);
+#define ill(u) scanf("%lld",&u);
+#define iull(u) scanf("%ulld",&u);
+#define inf(u) scanf("%f",&u);
+#define ild(u) scanf("%Llf",&u);
+#define ic(u) scanf("%c",&u); 
+#define is(u) scanf("%s",u);
+#define oi(u) printf("%d",u);
+#define oui(u) printf("%ud",u);
+#define oll(u) printf("%lld",u);
+#define oull(u) printf("%ulld",u);
+#define of(u) printf("%f",u);
+#define old(u) printf("%Llf",u);
+#define oc(u) printf("%c",u); 
+#define f(i,u,w) for(ll i=u;i<w;i++)
+#define f1(i,u,w) for(ll i=u;i>w;i--)
+#define AI(u,w) ll u[w]; f(i,0,w) ill(u[i]);
+#define AO(u,w) f(i,0,w) {oll(u[i]); printf(" ");}
+#define T ll t; cin>>t; while(t--)
+
+
+int main(){
+
+ /*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+
+
+}
