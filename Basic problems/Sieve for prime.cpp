5f53dda2ff6bdf2e45386117d6fcdfdec22c38ee
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-06-19 13:18:26 +0530

    first input no of test cases then no of lines

---
+++ Basic problems/Sieve for prime.cpp
@@ -0,0 +1,73 @@
+//SAURABHSAHAB
+//#include <boost/math/common_factor.hpp>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define full INT_MAX
+#define VL vector<ll>
+#define VS vector<string>
+#define LL list<ll> 
+#define SL set<ll>
+#define SC set<char>
+#define USL unordered_set<ll>
+#define USC unordered_set<char>
+#define MLL map<ll,ll>
+#define MCL map<char,ll>
+#define UMLL unordered_map<ll,ll>
+#define UMCL unordered_map<char,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tupple
+#define er equal_range
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define DO greater<ll>()
+#define pb push_back
+#define in insert
+#define pop pop_back()
+#define bg begin()
+#define ed end()
+#define F first
+#define S second 
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define f(i,s,n) for(ll i=s;i<n;i++)
+#define f1(i,s,n) for(ll i=s;i>n;i--)
+#define f2(s) for(auto E : s)
+#define fm(s) for(auto [x,y] : s)
+#define it(u) ::iterator E=u.bg
+#define AI(a,n) ll a[n]; f(i,0,n)cin>>a[i]
+#define AO(a,n) f(i,0,n)cout<<a[i]<<" "
+#define T ll t; cin>>t; while(t--)
+
+vector<bool> a(1000010,1);
+void sieve(){
+    a[0]=0;
+    a[1]=0;
+    for(ll i=2;i<1000010;i++){
+        if(a[i]){
+        for(ll j=i*i;j<1000010;j=j+i)
+                a[j]=0;
+        }
+    }
+}
+
+int main(){
+    
+/*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    sieve();
+    
+    T{
+        ll n;
+        cin>>n;
+        a[n]?cout<<"YES"<<"\n":cout<<"NO"<<"\n";
+    }
+}
