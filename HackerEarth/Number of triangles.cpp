cf79a25be272d9d9740811e43d7bd3cbb8b13182
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-06-27 22:48:07 +0530

    https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/number-of-triangles-6ac88eb2/description/
    https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/number-of-triangles-6ac88eb2/description/

---
+++ HackerEarth/Number of triangles.cpp
@@ -0,0 +1,97 @@
+//SAURABHSAHAB
+//#include <boost/math/common_factor.hpp>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long 
+#define ui unsigned int 
+#define ldb long double 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define full INT_MAX
+#define V vector 
+#define VL vector<ll>
+#define VS vector<string>
+#define L list
+#define LL list<ll> 
+#define D deque
+#define DL deque<ll>
+#define S set 
+#define SL set<ll>
+#define SC set<char>
+#define USL unordered_set<ll>
+#define USC unordered_set<char>
+#define M map
+#define MLL map<ll,ll>
+#define MCL map<char,ll>
+#define UMLL unordered_map<ll,ll>
+#define UMCL unordered_map<char,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tupple
+#define er equal_range
+#define lb lower_bound 
+#define up upper_bound 
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define DO greater<ll>()
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define all(u) ((u).bg,(u).ed)
+#define F first
+#define S second 
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define f(i,u,w) for(ll i=u;i<w;i++)
+#define f1(i,u,w) for(ll i=u;i>w;i--)
+#define f2(u) for(auto E : (u))
+#define fm(u) for(auto [x,y] : (u))
+#define it(u) ::iterator E=(u).bg
+#define AI(u,w) ll u[w]; f(i,0,w)cin>>u[i]
+#define AO(u,w) f(i,0,w)cout<<u[i]<<" "
+#define T ll t; cin>>t; while(t--)
+ 
+int main(){
+    
+/*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    T{
+        ll n,a,b,p,q,ans=0;
+        cin>>n>>a>>b;
+        p=abs(a-b)-1;
+        q=n-2-p;
+        if(q>2)
+        ans+=q-2;
+        if(p>2)
+        ans+=p-2;
+        if(q>0&&p>1){
+            ll r=p-1;
+            r=r*q;
+            ans+=r;
+        } 
+        if(p>0&&q>1){
+            ll r=q-1;
+            r=r*p;
+            ans+=r;
+        }
+        if(p>3){
+            ll r=(p-3)*(p-4);
+            r=r+(2*(p-3));
+            ans=ans+r;
+        }
+        if(q>3){
+            ll r=(q-3)*(q-4);
+            r=r+(2*(q-3));
+            ans=ans+r;
+        }
+        cout<<ans<<endl; 
+    }
+}
