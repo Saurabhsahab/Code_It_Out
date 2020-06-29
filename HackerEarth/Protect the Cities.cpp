6b89e0057e399d38aea48c0f562c119c8dfe5793
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-06-29 15:57:54 +0530

    https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/protect-the-cities-1/description/
    https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/protect-the-cities-1/description/

---
+++ HackerEarth/Protect the Cities.cpp
@@ -0,0 +1,84 @@
+//SAURABHSAHAB
+//#include <boost/math/common_factor.hpp>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long 
+#define ui unsigned int 
+#define ldb long double 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define full INT_MAX
+#define V vector 
+#define VL vector<ll>
+#define VS vector<string>
+#define L list
+#define LL list<ll> 
+#define D deque
+#define DL deque<ll>
+#define SL set<ll>
+#define SC set<char>
+#define USL unordered_set<ll>
+#define USC unordered_set<char>
+#define M map
+#define MLL map<ll,ll>
+#define MCL map<char,ll>
+#define UMLL unordered_map<ll,ll>
+#define UMCL unordered_map<char,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tupple
+#define er equal_range
+#define lb lower_bound 
+#define up upper_bound 
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define DO greater<ll>()
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define all(u) ((u).bg,(u).ed)
+#define F first
+#define S second 
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define f(i,u,w) for(ll i=u;i<w;i++)
+#define f1(i,u,w) for(ll i=u;i>w;i--)
+#define f2(u) for(auto E : (u))
+#define fm(u) for(auto [x,y] : (u))
+#define it(u) ::iterator E=(u).bg
+#define AI(u,w) ll u[w]; f(i,0,w)cin>>u[i]
+#define AO(u,w) f(i,0,w)cout<<u[i]<<" "
+#define T ll t; cin>>t; while(t--)
+
+int main(){
+    
+/*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+     T{
+        ll n,a=0,b=0,c=0,d=0,x=0,y=0,p=0,q=0;
+        cin>>n;
+        while(n--){
+         cin>>x>>y;
+         if(x>=0)
+         a=max(a,x);
+         else
+         c=min(c,x);
+         if(y>=0)
+         b=max(b,y);
+         else
+         d=min(d,y);
+        }
+         p=a-c;
+         q=b-d;
+         ll k=max(p,q);
+         cout<<k*k<<"\n";
+    } 
+}
