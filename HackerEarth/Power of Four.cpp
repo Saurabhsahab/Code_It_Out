892d9c9351f562cbb51087613d14cab08c23049a
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-07-14 14:26:09 +0530

    https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/power-of-four/description/?layout=old
    https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/power-of-four/description/?layout=old

--- HackerEarth/Power of Four.cpp
+++ HackerEarth/Power of Four.cpp
@@ -67,7 +67,7 @@ bool pf(ll x){
 
 int main(){
     
-/*	freopen("input.txt", "r" , stdin);
+/* 	freopen("input.txt", "r" , stdin);
 	freopen("output.txt", "w" , stdout);  */
     
     ios_base::sync_with_stdio(false);
