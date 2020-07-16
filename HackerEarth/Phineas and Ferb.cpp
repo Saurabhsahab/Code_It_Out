d8beefcecf513f7897124d74957c154841acd1fc
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-07-16 13:30:31 +0530

    https://www.hackerearth.com/problem/algorithm/stone-game-a39dbbe7/description/
    https://www.hackerearth.com/problem/algorithm/stone-game-a39dbbe7/description/

---
+++ HackerEarth/Phineas and Ferb.cpp
@@ -0,0 +1,80 @@
+//SAURABHSAHAB
+//#include <boost/math/common_factor.hpp>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long 
+#define ui unsigned int 
+#define ldb long double 
+#define lcm(x,y) boost::math::lcm(x,y)
+#define mod 1000000007
+#define mod1 998244353
+#define full INT_MAX
+#define V vector 
+#define VL vector<ll>
+#define VS vector<string>
+#define L list
+#define LL list<ll> 
+#define D deque
+#define DL deque<ll>
+#define SL set<ll>
+#define SC set<char>
+#define USL unordered_set<ll>
+#define USC unordered_set<char>
+#define M map
+#define MLL map<ll,ll>
+#define MCL map<char,ll>
+#define UMLL unordered_map<ll,ll>
+#define UMCL unordered_map<char,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tupple
+#define er equal_range
+#define lb lower_bound 
+#define up upper_bound 
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define DO greater<ll>()
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define all(u) ((u).bg,(u).ed)
+#define F first
+#define S second 
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define f(i,u,w) for(ll i=u;i<w;i++)
+#define f1(i,u,w) for(ll i=u;i>w;i--)
+#define f2(u) for(auto E : (u))
+#define fm(u) for(auto [x,y] : (u))
+#define it(u) ::iterator E=(u).bg
+#define AI(u,w) ll u[w]; f(i,0,w)cin>>u[i]
+#define AO(u,w) f(i,0,w)cout<<u[i]<<" "
+#define T ll t; cin>>t; while(t--)
+
+int main(){
+    
+/*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    ll n,a[n+1]={0},b;
+    cin>>n;
+    f(i,1,n+1){
+        cin>>b;
+        a[i]=a[i-1]^b;
+    }
+    T{
+        ll c,d;
+        cin>>c>>d;
+        if(a[d]^a[c-1])
+        cout<<"Phineas\n";
+        else
+        cout<<"Ferb\n";
+    }
+}
