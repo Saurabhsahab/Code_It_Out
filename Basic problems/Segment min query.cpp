d309b41b9fc73c02775cf9978800a8942f9f6112
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-11-09 03:17:36 +0530

    https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/tutorial/
    https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/tutorial/

---
+++ Basic problems/Segment min query.cpp
@@ -0,0 +1,181 @@
+/***
+⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀
+
+   * Author : SUARABH UPADHAYAY
+   * Institution : IET LUCKNOW
+   
+   
+***/
+
+#include<bits/stdc++.h>
+//#include <boost/math/common_factor.hpp>
+//#include <boost/multiprecision/cpp_int.hpp> 
+//using namespace boost::multiprecision; 
+using namespace std;
+#define lll cpp_int
+#define ull unsigned long long 
+#define ll long long 
+#define ui unsigned int 
+#define ldb long double 
+//#define lcm(o,g) boost::math::lcm(o,g)
+#define mod 1000000007
+#define mod1 1000003
+#define mod2 998244353
+#define pi acos(-1)
+#define full INT_MAX 
+#define llfull LLONG_MAX
+#define V vector 
+#define VL vector<ll>
+#define L list
+#define LL list<ll> 
+#define D deque
+#define DL deque<ll>
+#define PQL priority_queue<ll> 
+#define SL set<ll>
+#define SC set<char>
+#define USL unordered_set<ll>
+#define M map
+#define MLL map<ll,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tupple
+#define er equal_range
+#define lb lower_bound 
+#define ub upper_bound 
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define DO greater<ll>()
+#define DM greater<ll>
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define ed end()
+#define sz size()
+#define all(o) ((o).bg,(o).ed)
+#define F first
+#define S second 
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define cg(g) getline(cin,(g))
+#define f(g,h,o) for(ll g=h;g<o;g++)
+#define f1(g,h,o) for(ll g=h;g>o;g--)
+#define f2(g) for(auto E : (g))
+#define fm(g) for(auto [X,Y] : (g))
+#define it(g) ::iterator E=(g).bg
+#define AI(g,o) ll g[o]; f(i,0,o)cin>>g[i]
+#define AO(g,o) f(i,0,o)cout<<g[i]<<" "
+#define T ui t; cin>>t; while(t--)
+#define T1(g) ui g; cin>>g; while(g--)
+
+VL a(100001,0),b(400005,0);
+
+void bt(ll i,ll s,ll e){
+
+    if(s>e)
+    return;
+
+    if(s==e){
+    b[i]=a[s];
+    return;
+    }
+
+    ll m=(s+e)/2;
+
+    bt(2*i,s,m);
+    bt((2*i)+1,m+1,e);
+
+    b[i]=min(b[2*i],b[(2*i)+1]);
+}
+
+ll qu(ll i,ll l,ll r,ll s,ll e){
+    if(s>r||l>e)
+    return llfull;
+
+    if(s>=l&&e<=r)
+    return b[i];
+
+    ll m=(s+e)/2;
+
+    ll p=qu(2*i,l,r,s,m);
+    ll q=qu((2*i)+1,l,r,m+1,e);
+
+    return min(p,q);
+}
+
+void uu(ll i,ll y,ll z,ll s,ll e){
+    if(s>e)
+    return;
+
+    if(s==e){
+        a[y]=z;
+        b[i]=z;
+        return;
+    }
+    else{
+
+        ll m=(s+e)/2;
+
+        if(y<=m&&y>=s)
+        uu(2*i,y,z,s,m);
+        else
+        uu((2*i)+1,y,z,m+1,e);
+
+        
+        
+        b[i]=min(b[2*i],b[(2*i)+1]);
+    }
+}
+
+int main(){
+    
+/*	freopen("input.txt", "r" , stdin);
+	freopen("output.txt", "w" , stdout);  */
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    //cout<<fixed<<setprecision(10)
+    
+    ll n,q;
+    cin>>n>>q;
+
+    f(i,1,n+1){
+        ll x;
+        cin>>x;
+        a[i]=x;
+    }
+
+    bt(1,1,n);
+
+    while(q--){
+       char c;
+       cin>>c;
+
+       if(c=='q'){
+           ll y,z;
+           cin>>y>>z;
+           cout<<qu(1,y,z,1,n)<<"\n";
+       }
+       else{
+           ll y,z;
+           cin>>y>>z;
+           uu(1,y,z,1,n);
+       }
+    }
+}
